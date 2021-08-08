e085eea3db9c52e0615f890b81d3810618804276
Rafal <rafal.wp.poczta@wp.pl>
2021-08-08 20:36:45 +0200

    Added scroll animations

--- src/scripts.js
+++ src/scripts.js
@@ -2,3 +2,16 @@ import "./styles.scss";
 
 import "./sections/navbar/navbar.js";
 import "./sections/map/map.js"
+
+import AOS from 'aos';
+import 'aos/src/sass/aos.scss';
+
+if (window.innerWidth > 768) {
+
+    let sectionRows = document.querySelectorAll("section.container div.row");
+    [...sectionRows].forEach(section => {
+        section.setAttribute("data-aos", "zoom-out");
+    });
+
+    AOS.init();
+}
